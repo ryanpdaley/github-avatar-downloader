9b226b63dda563bce7dad9b3f1879869e1790306
Ryan Daley <rpdaley@gmail.com>
2017-03-13 21:57:05 +0000

    comments!

--- download_avatars.js
+++ download_avatars.js
@@ -4,12 +4,14 @@ var GITHUB_USER = "ryanpdaley";
 var GITHUB_TOKEN = "Y421c7222f0973fa813cb340417f656d8c4a62738";
 var AVATAR_DIR = './avatars/';
 
+//  Options for the requests
 var options = {
     headers: {
       'User-Agent': 'GitHub Avatar Downloader - Student Project'
     }
   };
 
+//  function to parse the URLs and usernames from the passed JSON
 function getImageURLs(jsonDump){
   avatarUrls = {};
   for (var key in jsonDump){
@@ -18,6 +20,7 @@ function getImageURLs(jsonDump){
   return avatarUrls;
 }
 
+//  function to actually download the images from a given URL to a given file path
 function downloadImageByURL(url, filePath) {
   options['url'] = url;
   request.get(url)
@@ -27,6 +30,7 @@ function downloadImageByURL(url, filePath) {
        .pipe(fs.createWriteStream(filePath));
 }
 
+//  callback to grab the info about the avatars
 function getAvatarInfo(error, response, body) {
   if (!error && response.statusCode == 200) {
     var info = JSON.parse(body);
@@ -44,8 +48,9 @@ function getAvatarInfo(error, response, body) {
   }
 }
 
-
-
+//  function to do the requests
+//  Note: my authentication returns a 401 error, but using no authentication works fine...
+//  I checked this with Erica, and she confirmed something was wonky with github auth, and not my code.
 function getRepoContributors(repoOwner, repoName, cb) {
   //var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var requestURL = 'https://api.github.com/repos/jquery/jquery/contributors';
@@ -54,6 +59,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, cb);
 }
 
+//  Arg parser - node download_avatars.js <owner> <repo>
 var args = process.argv.slice(2);
 if (args.length === 2) {
   getRepoContributors(args[0], args[1], getAvatarInfo);
