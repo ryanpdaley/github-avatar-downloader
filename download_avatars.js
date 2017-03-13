f332e7117e1577b0acf4074ba678ba793ba45a01
Ryan Daley <rpdaley@gmail.com>
2017-03-13 21:41:10 +0000

    now with CL args... authentication is wonky

--- download_avatars.js
+++ download_avatars.js
@@ -15,7 +15,6 @@ function getImageURLs(jsonDump){
   for (var key in jsonDump){
     avatarUrls[jsonDump[key].login] = jsonDump[key].avatar_url
   }
-  //console.log(avatarUrls)
   return avatarUrls;
 }
 
@@ -50,8 +49,14 @@ function getAvatarInfo(error, response, body) {
 function getRepoContributors(repoOwner, repoName, cb) {
   //var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var requestURL = 'https://api.github.com/repos/jquery/jquery/contributors'
+  //console.log(requestURL)
   options['url'] = requestURL
   request(options, cb);
 }
 
-getRepoContributors('jquery', 'jquery', getAvatarInfo)
+var args = process.argv.slice(2);
+if (args.length === 2) {
+  getRepoContributors(args[0], args[1], getAvatarInfo);
+} else {
+  console.log('Usage: node download_avatars.js <owner> <repo> - incorrect number of arguments');
+}
