b26ab44d165ec9e31025fa9a42c3a5a66af9f500
Ryan Daley <rpdaley@gmail.com>
2017-03-13 21:16:52 +0000

    it worked... authentication is wonky

--- download_avatars.js
+++ download_avatars.js
@@ -2,6 +2,7 @@ var request = require('request');
 var fs = require('fs');
 var GITHUB_USER = "ryanpdaley";
 var GITHUB_TOKEN = "Y421c7222f0973fa813cb340417f656d8c4a62738";
+var AVATAR_DIR = './avatars/'
 
 var options = {
     headers: {
@@ -10,15 +11,33 @@ var options = {
   };
 
 function getImageURLs(jsonDump){
+  avatarUrls = {};
   for (var key in jsonDump){
-    console.log(jsonDump[key].avatar_url)
+    avatarUrls[jsonDump[key].login] = jsonDump[key].avatar_url
   }
+  //console.log(avatarUrls)
+  return avatarUrls;
 }
 
-function callback(error, response, body) {
+function downloadImageByURL(url, filePath) {
+  options['url'] = url;
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
+function getAvatarInfo(error, response, body) {
   if (!error && response.statusCode == 200) {
     var info = JSON.parse(body);
-    getImageURLs(info);
+    var avatarUrls = getImageURLs(info);
+    console.log(avatarUrls);
+    for (var key in avatarUrls){
+      imgURL = avatarUrls[key]
+      imgFilePath = `./avatars/${key}.jpg`
+      downloadImageByURL(imgURL, imgFilePath)
+    }
   } else {
     console.log('Response Status Code: ', response.statusCode);
     console.log('Response Message: ', response.statusMessage);
@@ -32,20 +51,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   //var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   var requestURL = 'https://api.github.com/repos/jquery/jquery/contributors'
   options['url'] = requestURL
-  console.log(options)
-  var body = request(options, callback);
-//   request.get(requestURL)
-//         .set('User-Agent', macUAString)
-
-//        .on('error', function (err) {
-//          throw err;
-//        })
-//        .on('response', function (response) {
-//          console.log('Response Status Code: ', response.statusCode);
-//          console.log('Response Message: ', response.statusMessage);
-//          console.log('Response Content Type: ', response.headers['content-type']);
-//        });
-//        //.pipe(fs.createWriteStream('./future.jpg'));
+  request(options, cb);
 }
 
-getRepoContributors('jquery', 'jquery', 'callback')
+getRepoContributors('jquery', 'jquery', getAvatarInfo)
